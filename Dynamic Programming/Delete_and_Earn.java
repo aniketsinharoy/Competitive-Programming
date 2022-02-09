77bd5449052cc07aec4838d26870f277a346193e
Aniket Sinha Roy <73071212+aniketsinharoy@users.noreply.github.com>
2022-02-10 00:14:19 +0530

    Create Delete_and_Earn.java
    You are given an integer array nums. You want to maximize the number of points you get by performing the following operation any number of times:
    
    Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you must delete every element equal to nums[i] - 1 and every element equal to nums[i] + 1.
    Return the maximum number of points you can earn by applying the above operation some number of times.
    
    Example 1:
    Input: nums = [3,4,2]
    Output: 6
    Explanation: You can perform the following operations:
    - Delete 4 to earn 4 points. Consequently, 3 is also deleted. nums = [2].
    - Delete 2 to earn 2 points. nums = [].
    You earn a total of 6 points.
    
    Example 2:
    Input: nums = [2,2,3,3,3,4]
    Output: 9
    Explanation: You can perform the following operations:
    - Delete a 3 to earn 3 points. All 2's and 4's are also deleted. nums = [3,3].
    - Delete a 3 again to earn 3 points. nums = [3].
    - Delete a 3 once more to earn 3 points. nums = [].
    You earn a total of 9 points.

---
+++ Dynamic Programming/Delete_and_Earn.java
@@ -0,0 +1,49 @@
+class Delete_and_Earn
+{
+    public int deleteAndEarn(int[] nums) 
+    {
+         TreeMap<Integer,Integer> count = new TreeMap<>();
+        ArrayList<Integer> distinct_number = new ArrayList<>();
+        int i=0;
+        for (i = 0; i < nums.length; i++) 
+        {
+            if(count.containsKey(nums[i])==false)
+            {
+                count.put(nums[i],1);
+                distinct_number.add(nums[i]);
+            }
+            else
+            {
+                count.put(nums[i], count.get(nums[i])+1);
+            }    
+        }
+        if(distinct_number.size()==1)
+        {
+            return distinct_number.get(0)*count.get(distinct_number.get(0));
+        }
+        Collections.sort(distinct_number);
+        int[] ans = new int[distinct_number.size()];
+        ans[0]=distinct_number.get(0)*count.get(distinct_number.get(0));
+        if(distinct_number.get(1)-1!=distinct_number.get(0))
+        {
+            ans[1]=distinct_number.get(1)*count.get(distinct_number.get(1))+ans[0];
+        }
+        else
+        {
+            ans[1]=Math.max(distinct_number.get(1)*count.get(distinct_number.get(1)),ans[0]);
+        }
+        for(i=2;i<distinct_number.size();++i)
+        {
+            if(distinct_number.get(i)-1!=distinct_number.get(i-1))
+            {
+                ans[i]=distinct_number.get(i)*count.get(distinct_number.get(i))+ans[i-1];
+            }
+            else
+            {
+                ans[i]=distinct_number.get(i)*count.get(distinct_number.get(i))+ans[i-2];
+            }
+            ans[i]=Math.max(ans[i],ans[i-1]);
+        }
+        return ans[ans.length-1];
+    }
+}
