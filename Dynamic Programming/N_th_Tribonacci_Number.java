52bfc42713092c02c3128e385e23a04d7f609841
Aniket Sinha Roy <73071212+aniketsinharoy@users.noreply.github.com>
2022-02-10 00:11:25 +0530

    Create N_th_Tribonacci_Number.java
    The Tribonacci sequence Tn is defined as follows: 
    
    T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
    
    Given n, return the value of Tn.
    
    Example 1:
    Input: n = 4
    Output: 4
    Explanation:
    T_3 = 0 + 1 + 1 = 2
    T_4 = 1 + 1 + 2 = 4
    
    Example 2:
    Input: n = 25
    Output: 1389537

---
+++ Dynamic Programming/N_th_Tribonacci_Number.java
@@ -0,0 +1,26 @@
+class N_th_Tribonacci_Number
+{
+    public int tribonacci(int n) 
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        if(n<=2)
+        {
+            return 1;
+        }
+        int a=0;
+        int b=1;
+        int c=1;
+        int d=a+b+c;
+        for(int i=3;i<=n;++i)
+        {
+            d=c+b+a;
+            a=b;
+            b=c;
+            c=d;
+        }
+        return d;
+    }
+}
