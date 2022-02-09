8e21c19ba5f4cab9b20d63334a6b8dabf4089bee
Aniket Sinha Roy <73071212+aniketsinharoy@users.noreply.github.com>
2022-02-10 00:07:03 +0530

    Create Min_Cost_Climbing_Stairs.java
    You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
    You can either start from the step with index 0, or the step with index 1.
    Return the minimum cost to reach the top of the floor.
    
    Example 1:
    Input: cost = [10,15,20]
    Output: 15
    Explanation: You will start at index 1.
    - Pay 15 and climb two steps to reach the top.
    The total cost is 15.
    
    Example 2:
    Input: cost = [1,100,1,1,1,100,1,1,100,1]
    Output: 6
    Explanation: You will start at index 0.
    - Pay 1 and climb two steps to reach index 2.
    - Pay 1 and climb two steps to reach index 4.
    - Pay 1 and climb two steps to reach index 6.
    - Pay 1 and climb one step to reach index 7.
    - Pay 1 and climb two steps to reach index 9.
    - Pay 1 and climb one step to reach the top.
    The total cost is 6.

---
+++ Dynamic Programming/Min_Cost_Climbing_Stairs.java
@@ -0,0 +1,15 @@
+class Min_Cost_Climbing_Stairs
+{
+    public int minCostClimbingStairs(int[] cost) 
+    {
+       if(cost.length==1)
+        {
+            return 0;
+        }
+        for (int i = 2; i < cost.length; i++) 
+        {
+            cost[i]=Math.min(cost[i-1], cost[i-2])+cost[i]; 
+        }
+        return Math.min(cost[cost.length-1],cost[cost.length-2]);
+    }
+}
